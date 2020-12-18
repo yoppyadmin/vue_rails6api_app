03d56034160c94abeb01a4a8286f6d182455fdd2
yoppyadmin <yoppy.admigle@gmail.com>
2020-12-19 05:37:13 +0900

    model users 5

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,31 +1,44 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-red_avatar = File.open(Rails.root.join("public/red_avatar.jpeg"))
-blue_avatar = File.open(Rails.root.join("public/blue_avatar.jpeg"))
+url = "http://robohash.org/1.png"
+fileName = File.basename(url) # -> "1.png"
+dirName = "#{Rails.root}/public/seed_#{Rails.env}/" # -> "/app/backend/public/test/"
+filePath = dirName + fileName # -> "/app/backend/public/test/1.png"
+FileUtils.mkdir_p(dirName) unless FileTest.exist?(dirName)
+open(filePath, 'wb') do |output|
+  open(url) do |data|
+    output.write(data.read)
+  end
+end
 User.create!(
   name: "hogehoge",
   email: "hoge@hoge.com",
   password: "hogehoge",
   password_confirmation: "hogehoge",
-  avatar: red_avatar,
+  avatar: File.open(Rails.root.join(filePath)),
   admin: true
 )
 
-(2..70).each do |n|
+(2..50).each do |n|
   name  = Faker::Internet.username(specifier: 6..20, separators: %w[_ -])
   email = "example-#{n}@example.com"
   password = "password"
+  url = "http://robohash.org/#{n}.png"
+  fileName = File.basename(url)
+  dirName = "#{Rails.root}/public/seed_#{Rails.env}/"
+  filePath = dirName + fileName
+  FileUtils.mkdir_p(dirName) unless FileTest.exist?(dirName)
+  open(filePath, 'wb') do |output|
+    open(url) do |data|
+      output.write(data.read)
+    end
+  end
   User.create!(
     name: name,
     email: email,
     password: password,
     password_confirmation: password,
-    avatar: blue_avatar
+    avatar: File.open(Rails.root.join(filePath))
   )
 end
+
+# red_avatar = File.open(Rails.root.join("public/red_avatar.jpeg"))
+# blue_avatar = File.open(Rails.root.join("public/blue_avatar.jpeg"))
